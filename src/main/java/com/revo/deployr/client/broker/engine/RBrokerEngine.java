9d6029252105e6b8cc32fd3957dae4c5d2bebc4b
David Russell <david.russell@revolutionanalytics.com>
2015-01-30 16:35:17 +0700

    RW-599: Removed dependency on variable-time ConcurrentLinkedQueue.size() operation.
    Updated implementation of RBroker, replacing ConcurrentLinkedQueue.size() call with isEmpty() call to reduce runtime overhead.
    
    Per the JDK documentation on ConcurrentLinkedQueue:
    
    "Beware that, unlike in most collections, the size method is NOT a constant-time operation. Because of the asynchronous nature of these queues, determining the current number of elements requires an O(n) traversal. Additionally, if elements are added or removed during execution of this method, the returned result may be inaccurate. Thus, this method is typically not very useful in concurrent applications."

--- src/main/java/com/revo/deployr/client/broker/engine/RBrokerEngine.java
+++ src/main/java/com/revo/deployr/client/broker/engine/RBrokerEngine.java
@@ -611,7 +611,7 @@ public abstract class RBrokerEngine implements RBroker {
 
                 try {
 
-                    while (liveTaskTokens.size() == 0 &&
+                    while (liveTaskTokens.isEmpty() == 0 &&
                             taskBrokerIsActive.get()) {
                         try {
                             Thread.currentThread().sleep(
